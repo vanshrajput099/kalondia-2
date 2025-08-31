7e2c5f0cee304fff72d8b5bdcec57ff123a73a70
Vansh Rajput <135845531+vanshrajput099@users.noreply.github.com>
2025-08-31 21:35:57 +0530

    first commit

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -6,11 +6,11 @@ const page = () => {
     return (
         <div>
             <Image src={bg} className='h-[300px] object-cover' alt='image'/>
-            <div className='p-2 xl:p-0 xl:w-[60%] text-center mx-auto py-20'>
+            <div className='p-2 xl:py-20 xl:w-[60%] text-center mx-auto py-20'>
                 <h1 className='text-2xl xl:text-4xl text-[#6224A9] font-medium'>Investor Relations Inquiry</h1>
                 <form className='space-y-5 mt-20'>
 
-                    <div className='grid xl:grid-cols-3 gap-x-3 gap-y-3 justify-between'>
+                    <div className='grid grid-cols-1 lg:grid-cols-3 gap-x-3 gap-y-3 justify-between'>
                         <div className='border py-1 px-2 flex flex-col items-start rounded-sm border-gray-400'>
                             <label htmlFor="" className='text-gray-500 text-sm'>TITLE</label>
                             <input className='border-none w-full text-sm' type="text" />
@@ -25,7 +25,7 @@ const page = () => {
                         </div>
                     </div>
 
-                    <div className='grid xl:grid-cols-2 gap-x-3 justify-between'>
+                    <div className='grid grid-cols-1 lg:grid-cols-2 gap-x-3 gap-y-3 justify-between'>
                         <div className='border py-1 px-2 flex flex-col items-start rounded-sm border-gray-400'>
                             <label htmlFor="" className='text-gray-500 text-sm'>EMAIL</label>
                             <input className='border-none w-full text-sm' type="text" />
@@ -46,7 +46,7 @@ const page = () => {
                         <input className='border-none w-full text-sm' type="text" />
                     </div>
 
-                    <div className='grid grid-cols-2 gap-x-3 justify-between'>
+                    <div className='grid grid-cols-1 lg:grid-cols-2 gap-x-3 gap-y-3 justify-between'>
                         <div className='border py-1 px-2 flex flex-col items-start rounded-sm border-gray-400'>
                             <label htmlFor="" className='text-gray-500 text-sm'>CITY</label>
                             <input className='border-none w-full text-sm' type="text" />
@@ -57,7 +57,7 @@ const page = () => {
                         </div>
                     </div>
 
-                    <div className='grid grid-cols-2 gap-x-3 justify-between'>
+                    <div className='grid grid-cols-1 lg:grid-cols-2 gap-x-3 gap-y-3 justify-between'>
                         <div className='border py-1 px-2 flex flex-col items-start rounded-sm border-gray-400'>
                             <label htmlFor="" className='text-gray-500 text-sm'>ZIP CODE</label>
                             <input className='border-none w-full text-sm' type="text" />
@@ -68,7 +68,7 @@ const page = () => {
                         </div>
                     </div>
 
-                    <div className='grid grid-cols-2 gap-x-3 justify-between'>
+                    <div className='grid grid-cols-1 lg:grid-cols-2 gap-x-3 gap-y-3 justify-between'>
                         <div className='border py-1 px-2 flex flex-col items-start rounded-sm border-gray-400'>
                             <label htmlFor="" className='text-gray-500 text-sm'>PHONE</label>
                             <input placeholder='(000) 000-0000' className='border-none w-full text-sm placeholder:text-sm' type="text" />
@@ -82,16 +82,16 @@ const page = () => {
                     <p className='text-left text-sm'>By providing a telephone number and submitting this form, you are consenting to be contacted by phone or email in response to your Inquiry. Elliott will not send you an SMS text message. </p>
 
                     <div className='flex justify-start'>
-                        <button className='border border-[#6224A9] text-[#6224A9] px-10 py-3 rounded-sm hover:border-white hover:cursor-pointer'>Submit</button>
+                        <button className='border border-[#6224A9] w-full lg:w-fit text-[#6224A9] px-10 py-3 rounded-sm hover:border-white hover:cursor-pointer'>Submit</button>
                     </div>
                 </form>
             </div>
 
-            <div className='bg-[#F9FAFB] py-20'>
-                <div className='w-[60%] mx-auto'>
-                    <h1 className='text-4xl text-[#6224A9] font-medium'>Media Inquiry</h1>
-                    <form className='space-y-5 mt-20 '>
-                        <div className='grid grid-cols-2 gap-x-3 justify-between'>
+            <div className='bg-[#F9FAFB] py-10'>
+                <div className='p-2 xl:py-20 xl:w-[60%] text-center mx-auto'>
+                    <h1 className='text-2xl xl:text-4xl text-[#6224A9] font-medium'>Media Inquiry</h1>
+                    <form className='space-y-5 mt-20'>
+                        <div className='grid grid-cols-1 lg:grid-cols-2 gap-x-3 gap-y-3 justify-between'>
                             <div className='border py-1 px-2 flex flex-col items-start rounded-sm border-gray-400 bg-white'>
                                 <label htmlFor="" className='text-gray-500 text-sm'>FIRST NAME</label>
                                 <input className='border-none w-full text-sm' type="text" />
@@ -102,7 +102,7 @@ const page = () => {
                             </div>
                         </div>
 
-                        <div className='grid grid-cols-2 gap-x-3 justify-between'>
+                        <div className='grid grid-cols-1 lg:grid-cols-2 gap-x-3 gap-y-3 justify-between'>
                             <div className='border py-1 px-2 flex flex-col items-start rounded-sm border-gray-400 bg-white'>
                                 <label htmlFor="" className='text-gray-500 text-sm'>EMAIL</label>
                                 <input className='border-none w-full ' type="text" />
@@ -113,7 +113,7 @@ const page = () => {
                             </div>
                         </div>
 
-                        <div className='grid grid-cols-2 gap-x-3 justify-between'>
+                        <div className='grid grid-cols-1 lg:grid-cols-2 gap-x-3 gap-y-3 justify-between'>
                             <div className='border py-1 px-2 flex flex-col items-start rounded-sm border-gray-400 bg-white'>
                                 <label htmlFor="" className='text-gray-500 text-sm'>PHONE</label>
                                 <input placeholder='(000) 000-0000' className='border-none w-full text-sm placeholder:text-sm' type="text" />
@@ -127,7 +127,7 @@ const page = () => {
                         <p className='text-left text-sm'>By providing a telephone number and submitting this form, you are consenting to be contacted by phone or email in response to your Inquiry. Elliott will not send you an SMS text message. </p>
 
                         <div className='flex justify-start'>
-                            <button className='border border-[#6224A9] text-[#6224A9] px-10 py-3 rounded-sm hover:border-white hover:cursor-pointer'>Submit</button>
+                            <button className='border border-[#6224A9] w-full lg:w-fit text-[#6224A9] px-10 py-3 rounded-sm hover:border-white hover:cursor-pointer'>Submit</button>
                         </div>
                     </form>
                 </div>
